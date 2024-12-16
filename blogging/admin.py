c4d9ce7b379d54f8e10642b75eaa3a3c2efd841b
Schlages <bschlagel91@gmail.com>
2024-12-16 11:35:20 -0800

    assign 7

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from blogging.models import Post, Category
 
-# and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInline(admin.TabularInline):  # or admin.StackedInline
+    model = Category.posts.through  # Use the through table for the ManyToMany relationship
+    extra = 1
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+# Customize the Category admin
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
